cea699efb69293b63b30f04760fe05db8e2b6506
alec <alecandersen97@gmail.com>
2025-02-18 13:52:50 -0500

    Create main function in client.py

--- client.py
+++ client.py
@@ -1,19 +1,24 @@
 import zmq
 
 
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5559")
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5559")
 
-print("Enter 1 to receive the current date, or enter 2 to enter a date and calculate the difference.")
+    print("Enter 1 to receive the current date, or enter 2 to enter a date and calculate the difference.")
 
-while True:
-    first_choice = input("Enter 1 or 2: ")
-    if first_choice == '1':
-        socket.send_string("date")
-    if first_choice == '2':
-        second_choice = input("Enter a date in YYYY-MM-DD format: ")
-        socket.send_string(second_choice)
+    while True:
+        first_choice = input("Enter 1 or 2: ")
+        if first_choice == '1':
+            socket.send_string("date")
+        if first_choice == '2':
+            second_choice = input("Enter a date in YYYY-MM-DD format: ")
+            socket.send_string(second_choice)
 
-    response = socket.recv()
-    print(response.decode())
+        response = socket.recv()
+        print(response.decode())
+
+
+if __name__ == "__main__":
+    main()
